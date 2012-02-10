1a947f9d69570e4138df2c234018dd285eda21cc
Michael Carroll <carroll.michael@gmail.com>
2012-02-10 13:32:13 -0600

    stub out headers

---
+++ homework2/include/shelby/person.h
@@ -0,0 +1,14 @@
+#ifndef PERSON_H
+#define PERSON_H
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+namespace person {
+
+}
+
+#endif
---
+++ homework2/include/shelby/shelby.h
@@ -0,0 +1,14 @@
+#ifndef SHELBY_H
+#define SHELBY_H
+
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+namespace shelby {
+
+}
+
+#endif
